662c01a54467eddee13213c05b64300e78df1df3
Django <django@Zhipings-MacBook-Pro.local>
2020-01-20 09:48:07 -0800

    update whitenoise again settings

--- InstaJZ2/settings.py
+++ InstaJZ2/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
